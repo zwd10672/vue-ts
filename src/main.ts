ef15dd51fa8e2eb66124c2b4256a3b535661e507
zwd <627806548@qq.com>
2023-11-23 21:17:23 +0800

    基本结构设计

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,20 @@
 import { createApp } from 'vue'
-import './style.css'
+import './assets/css/style'
+import 'vant/lib/index.css'
 import App from './App.vue'
 import store from './store/index'
 import router from './router'
+import './utils/rem'
 const app = createApp(App)
 // 使用vant组件
-import { Toast, showToast } from 'vant'
+import { Button, NavBar, Tabbar, TabbarItem, Checkbox, Toast, Icon } from 'vant'
+app.use(Button)
+app.use(NavBar)
+app.use(Tabbar)
+app.use(TabbarItem)
+app.use(Checkbox)
 app.use(Toast)
+app.use(Icon)
 
 app.use(store)
 app.use(router)
